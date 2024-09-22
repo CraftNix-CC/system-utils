42a1bca5e561eb4b854b244a2e2810c4037e8c06
WindClan <monstertoast2@gmail.com>
2024-09-22 00:51:51 -0400

    current version

---
+++ README.md
@@ -0,0 +1,4 @@
+# system-utils
+
+
+Commands that are essential to the system and shouldn't be included with the shell
---
+++ useradd.lua
@@ -0,0 +1,16 @@
+local sha256 = dofile("/lib/sha256.lua")
+
+local function main()
+  if not user.chkRoot() then print("This command requires root.") return 0 end
+	local success = false
+	print("")
+	term.write("Username: ")
+	local username = read()
+	term.write("Password: ")
+	local password = sha256(read("*"))
+	success = user.createUser(username,password)
+	if success then print("User created.") else print("User creation failed.") end
+	os.run({},"/bin/sh.lua")
+end
+
+main()
